ad4e6f73344fcf3d18cdbfcfd2c98785ebf44233
Ehsaan Ali <38082320+EhsaanAli97@users.noreply.github.com>
2020-07-10 00:14:27 +0100

    E-Commerce store

---
+++ src/data.js
@@ -0,0 +1,111 @@
+export const storeProducts = [
+	{
+		id: 1,
+		title: 'Google Pixel - Black',
+		img: 'img/product-1.png',
+		price: 10,
+		company: 'GOOGLE',
+		info:
+			'Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.',
+		inCart: false,
+		count: 0,
+		total: 0,
+	},
+	{
+		id: 2,
+		title: 'Samsung S7',
+		img: 'img/product-2.png',
+		price: 16,
+		company: 'SAMSUNG',
+		info:
+			'Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.',
+		inCart: false,
+		count: 0,
+		total: 0,
+	},
+	{
+		id: 3,
+		title: 'HTC 10 - Black',
+		img: 'img/product-3.png',
+		price: 8,
+		company: 'htc',
+		info:
+			'Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.',
+		inCart: false,
+		count: 0,
+		total: 0,
+	},
+	{
+		id: 4,
+		title: 'HTC 10 - White',
+		img: 'img/product-4.png',
+		price: 18,
+		company: 'htc',
+		info:
+			'Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.',
+		inCart: false,
+		count: 0,
+		total: 0,
+	},
+	{
+		id: 5,
+		title: 'HTC Desire 626s',
+		img: 'img/product-5.png',
+		price: 24,
+		company: 'htc',
+		info:
+			'Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.',
+		inCart: false,
+		count: 0,
+		total: 0,
+	},
+	{
+		id: 6,
+		title: 'Vintage Iphone',
+		img: 'img/product-6.png',
+		price: 17,
+		company: 'apple',
+		info:
+			'Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.',
+		inCart: false,
+		count: 0,
+		total: 0,
+	},
+	{
+		id: 7,
+		title: 'Iphone 7',
+		img: 'img/product-7.png',
+		price: 30,
+		company: 'apple',
+		info:
+			'Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.',
+		inCart: false,
+		count: 0,
+		total: 0,
+	},
+	{
+		id: 8,
+		title: 'Smashed Iphone',
+		img: 'img/product-8.png',
+		price: 2,
+		company: 'apple',
+		info:
+			'Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.',
+		inCart: false,
+		count: 0,
+		total: 0,
+	},
+];
+
+export const detailProduct = {
+	id: 1,
+	title: 'Google Pixel - Black',
+	img: 'img/product-1.png',
+	price: 10,
+	company: 'google',
+	info:
+		'The Google Pixel is a smartphone with a splash and dust resistant Quite Black finish, and a 5.0 inch FHD AMOLED display with 441 ppi. It is powered by a 2.15 GHz quad-core processor with 4 GB RAM, and comes with Android 7.1 Nougat. The phone features sensors for fingerprints, gyro, and proximity as well as an accelerometer, a compass, and a barometer. The non-removable Li-Ion 2,770 mAh battery provides enough power to support up to 26 hours of talk time, and up to 110 hours of music playback. A 12.3 MP camera with geo-tagging, touch focus, and face detection captures sharp pictures, while an 8 MP front facing camera allows high quality video calls. The Google Pixel measures 143.8 x 69.5 x 8.5 mm, and has an internal memory of 128 GB. It is unlocked, thus usable with any carrier.',
+	inCart: false,
+	count: 0,
+	total: 0,
+};
